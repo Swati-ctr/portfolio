132a56ea53f6a454dfa25cce54baa0fc83d0d322
Swati-ctr <swatichoudhary106@gmail.com>
2025-03-11 01:48:31 +0530

    Updated repository link

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav ul li a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetSection = document.querySelector(this.getAttribute("href"));
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+});
